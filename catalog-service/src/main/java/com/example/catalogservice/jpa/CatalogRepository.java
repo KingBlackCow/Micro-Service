b3c3c92160db45ae23ad3fcae6f28dbabaf51f29
KingBlackCow <sgs1159@naver.com>
2021-10-23 01:34:19 +0900

    feat : order 컨트롤러 젲가

--- catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
+++ catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
@@ -1,6 +1,8 @@
 package com.example.catalogservice.jpa;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.CrudRepository;
+
 
 public interface CatalogRepository extends CrudRepository<CatalogEntity,Long> {
     CatalogEntity findByProductId(String productId);
--- catalog-service/src/main/java/com/example/catalogservice/service/CatalogServiceImpl.java
+++ catalog-service/src/main/java/com/example/catalogservice/service/CatalogServiceImpl.java
@@ -1,2 +1,26 @@
-package com.example.catalogservice.service;public class CatalogServiceImpl {
+package com.example.catalogservice.service;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.jpa.CatalogRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+@Slf4j
+public class CatalogServiceImpl implements CatalogService{
+    private final CatalogRepository catalogRepository;
+
+    @Autowired
+    public CatalogServiceImpl(CatalogRepository catalogRepository) {
+        this.catalogRepository = catalogRepository;
+    }
+
+    @Override
+    public Iterable<CatalogEntity> getAllCatalogs() {
+        return catalogRepository.findAll();
+    }
 }
